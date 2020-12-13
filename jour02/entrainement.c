541d8510c18f54df1d058750dd971987a29a8f64
Benjamin I <benj.ifrah@gmail.com>
2020-12-13 22:59:43 +0100

    Initiation au C

---
+++ jour02/entrainement.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+void ft_print_alphabet(void)
+{
+	char letter = 'z';
+	while(letter != 96)
+	{
+		write(1, &letter, 1);
+		letter = letter - 1;
+	}
+}
+
+int main()
+{
+	ft_print_alphabet();
+}
