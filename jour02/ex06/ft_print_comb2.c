f51566e0461d19b089f5ea0a3ac261fa2283a523
Benjamin I <benj.ifrah@gmail.com>
2021-02-07 18:46:32 +0100

    Ajout ex06

---
+++ jour02/ex06/ft_print_comb2.c
@@ -0,0 +1,34 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_print_comb2(void)
+{
+	int a = 0, b = 1;
+	while(a <= 99 )
+	{
+		while(b <= 99 )
+		{
+			ft_putchar(a / 10 + '0');
+			ft_putchar(a % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(b / 10 + '0');
+			ft_putchar(b % 10 + '0');
+			if(a < 98)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			b++;
+		}
+		a++;
+		b = a + 1;
+	}
+}
+int main()
+{
+	ft_print_comb2();
+}
