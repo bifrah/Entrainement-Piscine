21f95542477a73e6129bdc7ff25970c20b8a07dc
Benjamin I <benj.ifrah@gmail.com>
2021-02-18 15:32:12 +0100

    Ajout c03, ex00 à ex03

---
+++ c03/ex02/ft_strcat.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i] != '\0')
+	{
+		i++;
+	}
+
+	return(i);
+}
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = ft_strlen(dest);
+	j = 0;
+	while(src[j])
+	{
+		dest[i++] = src[j++];
+	}
+	return(dest);
+}
+
+int main()
+{
+        char *dest = (char *)malloc(sizeof(char *) * (11));
+        char *src = "ben";
+        dest[0] = 'b';
+        dest[1] = 'o';
+        dest[2] = 'n';
+        dest[3] = 'j';
+        dest[4] = 'o';
+        dest[5] = 'u';
+        dest[6] = 'r';
+        dest[7] = '\0';
+        printf("%s\n", ft_strcat(dest, src));
+}
---
+++ c03/ex03/ft_strncat.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while(str[i] != '\0')
+	{
+		i++;
+	}
+
+	return(i);
+}
+
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	int i;
+	unsigned int j;
+
+	i = ft_strlen(dest);
+	j = 0;
+	while(src[j] && j < nb)
+	{
+		dest[i++] = src[j++];
+	}
+	return(dest);
+}
+
+int main()
+{
+        char *dest = (char *)malloc(sizeof(char *) * (11));
+        char *src = "ben";
+        dest[0] = 'b';
+        dest[1] = 'o';
+        dest[2] = 'n';
+        dest[3] = 'j';
+        dest[4] = 'o';
+        dest[5] = 'u';
+        dest[6] = 'r';
+        dest[7] = '\0';
+        printf("%s\n", ft_strncat(dest, src, 2));
+}
